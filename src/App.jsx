3c55f14bf1850fdea80da3019745c9083d417d41
Ajay Vishwakarma <adamyatech8@gmail.com>
2026-05-14 14:08:21 +0530

    portfolio updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import { Services } from './Components/Services'
 import { Work } from './Components/Work'
 import { About } from './Components/About'
 import { Toaster } from "sonner";
-import WhatsAppButton from "./components/WhatsAppButton";
+import Wbtn from './Components/Wbtn';
+
 
 import "./index.css";
 
@@ -15,7 +16,7 @@ const App = () => {
       <>
     <Toaster position="top-right" />  
     <div>
-      <WhatsAppButton />
+     <Wbtn />
       <Navbar />
       <Hero />
       <Services />
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { Services } from "@/components/Services";
-import { Work } from "@/components/Work";
-import { About } from "@/components/About";
-import { Contact } from "@/components/Contact";
-import SEO from "@/components/SEO";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { Services } from "./components/Services";
+import { Work } from "./components/Work";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import Seo from ".Components/Seo";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -14,7 +14,7 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
       <>
-      <SEO
+      <Seo
         title="AdamyaTech | Modern Web & App Development"
         description="Professional portfolio showcasing premium websites, app development, UI/UX projects, and modern tech solutions."
       />
